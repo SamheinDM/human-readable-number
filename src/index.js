7f17e1566f2b76ba6e34e966b99d3d76316abad1
SamheinDM <SamheinDM@yandex.ru>
2020-10-05 14:44:42 +0300

    init: start human-readable-number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function toReadable (number) {
-  
+    let num = number.toString(10).split('');
+    let numArr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let tenNum = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let decNum = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (num.length === 1 && num[0] == 0) {
+        return numArr[0];
+    }
 }
