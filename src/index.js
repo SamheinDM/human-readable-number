d9eeeb2693394a7d094d62734165b144a3c5d5e2
SamheinDM <SamheinDM@yandex.ru>
2020-10-06 15:06:40 +0300

    feat: add functionalyti

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 module.exports = function toReadable (number) {
     let num = number.toString(10).split('');
-    let numArr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    let tenNum = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let numArr = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let tenNum = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     let decNum = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let result = '';
 
-    if (num.length === 1 && num[0] == 0) {
-        return numArr[0];
+    if (number === 0) return 'zero';
+
+    if (num.length == 1) {
+        return numArr[num[0]];
+    } else if (num.length == 2) {
+        if (num[0] == 1) {
+            return tenNum[num[1]];
+        }
+        result = `${decNum[num[0]]} ${numArr[num[1]]}`;
+        return result;
+    } else {
+        if (num[1] == 1) {
+            result = `${numArr[num[0]]} hundred ${tenNum[num[1]]}`;
+            return result;
+        }
+        result = `${numArr[num[0]]} hundred ${decNum[num[1] - 2]} ${numArr[num[2]]}`;
+        return result;
     }
 }
