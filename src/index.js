6f7e99f65c626ff5f9664e04737efaf38df66da2
SamheinDM <SamheinDM@yandex.ru>
2020-10-07 14:47:14 +0300

    fix: fix algorithm

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ module.exports = function toReadable (number) {
     let num = number.toString(10).split('');
     let numArr = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
     let tenNum = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    let decNum = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let decNum = ['', '', 'twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety '];
     let result = '';
 
     if (number === 0) return 'zero';
@@ -13,14 +13,14 @@ module.exports = function toReadable (number) {
         if (num[0] == 1) {
             return tenNum[num[1]];
         }
-        result = `${decNum[num[0]]} ${numArr[num[1]]}`;
-        return result;
+        result = `${decNum[num[0]]}${numArr[num[1]]}`;
+        return result.trimRight();
     } else {
         if (num[1] == 1) {
-            result = `${numArr[num[0]]} hundred ${tenNum[num[1]]}`;
-            return result;
+            result = `${numArr[num[0]]} hundred ${tenNum[num[2]]}`;
+            return result.trimRight();
         }
-        result = `${numArr[num[0]]} hundred ${decNum[num[1] - 2]} ${numArr[num[2]]}`;
-        return result;
+        result = `${numArr[num[0]]} hundred ${decNum[num[1]]}${numArr[num[2]]}`;
+        return result.trimRight();
     }
 }
